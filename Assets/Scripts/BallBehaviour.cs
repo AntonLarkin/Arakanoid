32a1c9fa6288f57a112d80bdae810af8b9415e24
Anton Larkin <anton11061998@gmail.com>
2021-05-02 11:42:57 +0300

    init base logic

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 direction;
+    [SerializeField] private GameObject padTransform;
+
+    private bool isLaunched;
+
+
+    #endregion
+
+    #region Unity lifecycle
+
+    private void Update()
+    {
+        if (!isLaunched)
+        {
+            Vector3 padPosition = padTransform.transform.position;
+            padPosition.y = transform.position.y;
+
+            transform.position = padPosition;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                LaunchBall();
+            }
+        }
+
+    }
+
+    #endregion
+
+
+    #region Private methods
+
+    private void LaunchBall()
+    {
+        Vector2 force = direction * speed;
+        rb.AddForce(force);
+        isLaunched = true;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/PadBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadBehaviour : MonoBehaviour
+{
+    #region Unity lifecycle
+
+    private void Update()
+    {
+        Vector3 positionInPixels = Input.mousePosition;
+        Vector3 positionInWorld = Camera.main.ScreenToWorldPoint(positionInPixels);
+
+        Vector3 padPosition = positionInWorld;
+        padPosition.y = transform.position.y;
+        padPosition.z = 0f;         //хз,прочто почему то во время игры пропадает пад (?)
+
+        transform.position = padPosition;
+    }
+
+    #endregion
+
+}
